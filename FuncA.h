fb8ddeeaf48d93dbc95ba0526aa834ad3e4db6ca
Danyl Rymar <rymar.danyl@lll.kpi.ua>
2025-01-08 14:37:31 +0000

    Initial commit

---
+++ FuncA.h
@@ -0,0 +1,12 @@
+#ifndef FUNCA_H
+#define FUNCA_H
+
+class FuncA {
+public:
+    // Початкова реалізація: тригонометрична функція, яка повертає 0
+    double calculate() {
+        return 0;
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "FuncA.h"
+
+int main() {
+    FuncA func;
+    std::cout << "Initial function result: " << func.calculate() << std::endl;
+    return 0;
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
