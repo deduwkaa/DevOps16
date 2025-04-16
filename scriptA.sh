f40951f4956ed001bc9ec8b374dd9d75206bf6f0
deduwkaa <149321174+deduwkaa@users.noreply.github.com>
2025-04-17 00:21:42 +0300

    Update scriptA.sh

--- scriptA.sh
+++ scriptA.sh
@@ -4,30 +4,29 @@ IMAGE="deduwka/devops-16:latest"
 CPU_CORES=(0 1 2)
 CONTAINERS=("srv1" "srv2" "srv3")
 PORTS=(8081 8082 8083)
-CHECK_INTERVAL=30
+CHECK_INTERVAL=10
 BUSY_THRESHOLD=70.0
-IDLE_THRESHOLD=10.0
-NETWORK="my-network"
+IDLE_THRESHOLD=5.0
 
 launch_container() {
     local name=$1
     local core=$2
     local port=$3
-    echo "[INFO] Launching container $name on CPU core#$core..."
-    docker run -d --name $name --cpuset-cpus=$core --network=$NETWORK -p $port:8080 $IMAGE
+    echo "Launching container $name on CPU core#$core..."
+    docker run -d --name $name --cpuset-cpus=$core -p $port:8080 $IMAGE
 
-    echo "[INFO] Waiting 15 seconds for container $name to warm up..."
+    echo "Waiting 15 seconds to build docker"
     sleep 15
 }
 
 get_cpu_usage() {
     local name=$1
-    docker stats --no-stream --format "{{.CPUPerc}}" $name | sed 's/%//' | cut -d'.' -f1
+    docker stats --no-stream --format "{{.CPUPerc}}" $name | sed 's/%//'
 }
 
 stop_container() {
     local name=$1
-    echo "[INFO] Stopping container $name..."
+    echo "Stopping container $name..."
     docker kill --signal=SIGINT $name
     docker wait $name
     docker rm $name
@@ -43,32 +42,31 @@ while true; do
 
         if [[ $(docker ps -q --filter "name=$name") ]]; then
             cpu_usage=$(get_cpu_usage "$name")
-            echo "[INFO] Container $name: CPU usage = $cpu_usage%"
+            echo "Container $name: CPU usage = $cpu_usage%"
 
-            if (( cpu_usage > BUSY_THRESHOLD )) && [[ $i -lt 2 ]]; then
+            if (( $(echo "$cpu_usage > $BUSY_THRESHOLD" | bc -l) )) && [[ $i -lt 2 ]]; then
                 next_name="${CONTAINERS[$i+1]}"
-                if ! [[ " ${active_containers[*]} " == *" $next_name "* ]]; then
+                if ! [[ "${active_containers[@]}" =~ $next_name ]]; then
                     launch_container "$next_name" "${CPU_CORES[$i+1]}" "${PORTS[$i+1]}"
                     active_containers+=("$next_name")
                 fi
             fi
 
-            if (( cpu_usage < IDLE_THRESHOLD )) && [[ $i -gt 0 ]]; then
-                echo "[INFO] Container $name is idle. Stopping..."
+            if (( $(echo "$cpu_usage < $IDLE_THRESHOLD" | bc -l) )) && [[ $i -gt 0 ]]; then
+                echo "Container $name is idle. Stopping..."
                 stop_container "$name"
                 active_containers=("${active_containers[@]/$name}")
             fi
         fi
     done
 
-    echo "[INFO] Checking for image updates..."
+    echo "Checking for image updates..."
     pullResult=$(docker pull $IMAGE | grep "Downloaded newer image")
     if [[ -n "$pullResult" ]]; then
-        echo "[INFO] New image update found. Restarting containers..."
+        echo "New image update found. Restarting containers..."
         for name in "${active_containers[@]}"; do
-            index=$(echo "${CONTAINERS[@]}" | tr ' ' '\n' | grep -n "^$name$" | cut -d':' -f1)
-            index=$((index - 1))
             stop_container "$name"
+            index=${!CONTAINERS[@]}
             launch_container "$name" "${CPU_CORES[$index]}" "${PORTS[$index]}"
         done
     fi
