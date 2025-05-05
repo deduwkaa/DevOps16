80e6a9d49b6dc808cad971bf0ac5fc7ece117e6b
deduwkaa <149321174+deduwkaa@users.noreply.github.com>
2025-05-05 22:53:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,17 +3,6 @@
 
 int main() {
     FuncA func;
-    int n;
-    double x;
-    std::cout << "Enter the number of terms (n): ";
-    std::cin >> n;
-    std::cout << "Enter the value of x: ";
-    std::cin >> x;
-
-    std::cout << "Function result: " << func.calculate(n, x) << std::endl;
+    std::cout << "FuncA result: " << func.calculate(5, 1.0) << std::endl;
     return 0;
 }
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
